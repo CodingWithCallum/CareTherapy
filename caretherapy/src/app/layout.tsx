215a0bb7da8584acbe67dd6be888bce6f49f8db7
Callum Barry <codingwithcallum@gmail.com>
2025-10-09 17:23:12 +0200

    working

--- caretherapy/src/app/layout.tsx
+++ caretherapy/src/app/layout.tsx
@@ -31,7 +31,11 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen">
           <Navbar/>
           <main className="flex-1">
+          <div className="min-h-screen w-full bg-white relative overflow-hidden">
+          {/* Blue Corner Glow Background */}
+            <div className="absolute inset-0 z-0" style={{backgroundImage: `radial-gradient(circle 600px at 0% 200px, #bfdbfe, transparent), radial-gradient(circle 600px at 100% 200px, #bfdbfe, transparent)`,}}/>
             {children}
+          </div>
           </main>
           <Footer/>
         </div>
--- caretherapy/src/app/page.tsx
+++ caretherapy/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { HeroSectionOne } from "@/components/home/HeroSection";
+import {MarqueeDemo} from "@/components/home/TestimonialHome";
 
 export default function Home() {
   return (
     <div >
       <main >
+        <HeroSectionOne/>
+        <MarqueeDemo/>
       </main>
       <footer >
 
